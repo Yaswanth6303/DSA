f890f2900b0df35940bfdf7f81ae31a27c21349b
Yaswanth Gudivada <144692822+Yaswanth6303@users.noreply.github.com>
2025-06-17 22:05:56 +0530

    Add TC, SC and rectifies errors in the code

--- AdvancedProblems/Arrays/src/faqHard/MaxProductSubArray_6.java
+++ AdvancedProblems/Arrays/src/faqHard/MaxProductSubArray_6.java
@@ -1,9 +1,13 @@
 package faqHard;
 
 public class MaxProductSubArray_6 {
+    /**
+     * Time Complexity: O(n^3)
+     * Space Complexity: O(1)
+     */
     public int maxProductBrute(int[] nums) {
         int n = nums.length;
-        int maxProduct = 0;
+        int maxProduct = Integer.MIN_VALUE;
 
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
@@ -11,31 +15,38 @@ public class MaxProductSubArray_6 {
                 for (int k = i; k <= j; k++) {
                     product *= nums[k];
                 }
-                maxProduct = Math.max(product, maxProduct);
+                maxProduct = Math.max(maxProduct, product);
             }
         }
 
         return maxProduct;
     }
+    /**
+     * Time Complexity: O(n^2)
+     * Space Complexity: O(1)
+     */
     public int maxProductBetter(int[] nums) {
         int n = nums.length;
-        int maxProduct = 0;
+        int maxProduct = Integer.MIN_VALUE;
 
         for (int i = 0; i < n; i++) {
             int product = 1;
             for (int j = i; j < n; j++) {
                 product *= nums[j];
+                maxProduct = Math.max(maxProduct, product);
             }
-            maxProduct = Math.max(product, maxProduct);
         }
 
         return maxProduct;
     }
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
     public int maxProductOptimal(int[] nums) {
         int n = nums.length;
         int maxProduct = Integer.MIN_VALUE;
-        int prefix = 1;
-        int suffix = 1;
+        int prefix = 1, suffix = 1;
 
         for (int i = 0; i < n; i++) {
             // When 0 is in array product becomes 0 make it 1;
@@ -45,14 +56,15 @@ public class MaxProductSubArray_6 {
             prefix *= nums[i];
             suffix *= nums[n - 1 - i];
 
-            maxProduct = Math.max(prefix, suffix);
+            maxProduct = Math.max(maxProduct, Math.max(prefix, suffix));
         }
 
         return maxProduct;
     }
     public static void main(String[] args) {
         MaxProductSubArray_6 solver = new MaxProductSubArray_6();
-        int[] nums = {4, 5, 3, 7, 1, 2};
+        int[] nums = {2, 3, -2, 4};
+
         System.out.print("Max Product Brute: ");
         int maxProductBrute = solver.maxProductBrute(nums);
         System.out.println(maxProductBrute);
@@ -62,7 +74,7 @@ public class MaxProductSubArray_6 {
         System.out.println(maxProductBetter);
 
         System.out.print("Max Product Optimal: ");
-        int maxProductOptimal = solver.maxProductBetter(nums.clone());
+        int maxProductOptimal = solver.maxProductOptimal(nums.clone());
         System.out.println(maxProductOptimal);
     }
 }
