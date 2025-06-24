14c767cf52d88a4256f90e2796dd761dcda627c5
Yaswanth Gudivada <144692822+Yaswanth6303@users.noreply.github.com>
2025-06-24 13:27:40 +0530

    Add another brute method for Square root of the number code

--- AdvancedProblems/BinarySearch/src/onanswers/SquareRootNumber_1.java
+++ AdvancedProblems/BinarySearch/src/onanswers/SquareRootNumber_1.java
@@ -5,7 +5,7 @@ public class SquareRootNumber_1 {
      * Time Complexity: O(sqrt(n))
      * Space Complexity: O(1)
      */
-    public long floorSqrtBrute(long n) {
+    public long floorSqrtBruteM1(long n) {
         int result = 1;
 
         for (int i = 1; i <= n; i++) {
@@ -14,6 +14,23 @@ public class SquareRootNumber_1 {
         }
 
         return result;
+    }
+    /**
+     * Time Complexity: O(sqrt(n))
+     * Space Complexity: O(1)
+     */
+    public long floorSqrtBruteM2(long n) {
+        if (n == 0 || n == 1) return n;
+
+        for (long i = 1; i <= n / i; i++) {
+            long value = i * i;
+            if (value == n) {
+                return i;
+            } else if ((i + 1) > n / (i + 1)) { // Checking (i + 1)^2 > n
+                return i;
+            }
+        }
+        return -1;
     }
     /**
      * Time Complexity: O(log n)
@@ -40,9 +57,13 @@ public class SquareRootNumber_1 {
         SquareRootNumber_1 solver = new SquareRootNumber_1();
         long number = 28;
 
-        System.out.print("Square root of the number is(Brute): ");
-        long sqrtNumberBrute = solver.floorSqrtBrute(number);
-        System.out.println(sqrtNumberBrute);
+        System.out.print("Square root of the number is (BruteM1): ");
+        long sqrtNumberBruteM1 = solver.floorSqrtBruteM1(number);
+        System.out.println(sqrtNumberBruteM1);
+
+        System.out.print("Square root of the number is (BruteM2): ");
+        long sqrtNumberBruteM2 = solver.floorSqrtBruteM2(number);
+        System.out.println(sqrtNumberBruteM2);
 
         System.out.print("Square root of the number is(Optimal): ");
         long sqrtNumberOptimal = solver.floorSqrtOptimal(number);
