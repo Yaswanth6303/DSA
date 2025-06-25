a14d39ddbac2ac9050aed2729ca25729f14ab2f7
Yaswanth Gudivada <144692822+Yaswanth6303@users.noreply.github.com>
2025-06-25 16:36:22 +0530

    Edit Split array largest sum code

--- AdvancedProblems/BinarySearch/src/faqs/SplitArrayLargestSum_7.java
+++ AdvancedProblems/BinarySearch/src/faqs/SplitArrayLargestSum_7.java
@@ -1,52 +1,68 @@
 package faqs;
 
 public class SplitArrayLargestSum_7 {
-    private int countPartitions(int[] nums, int maxSum) {
+    /**
+     * Time Complexity: O(n * log(sum - max))
+     *   - n = length of array
+     *   - log(sum - max): binary search range
+     *
+     * Space Complexity: O(1)
+     */
+
+    public int splitArray(int[] nums, int k) {
+        int[] sumAndMax = findMaxAndSum(nums);
+        int low = sumAndMax[0]; // max of nums
+        int high = sumAndMax[1]; // sum of nums
+        int result = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (countSubArrays(nums, mid) <= k) {
+                result = mid;
+                high = mid - 1; // try smaller max
+            } else {
+                low = mid + 1; // too many splits, increase limit
+            }
+        }
+
+        return result;
+    }
+    public int[] findMaxAndSum(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+            max = Math.max(max, num);
+        }
+
+        return new int[]{max, sum};
+    }
+    public int countSubArrays(int[] nums, int maxAllowedSum) {
         int n = nums.length;
-        int partitions = 1;
-        long subarraySum = 0;
+        int subarrays = 1;
+        int currentSum = 0;
 
         for (int i = 0; i < n; i++) {
-            if (subarraySum + nums[i] <= maxSum) {
-                subarraySum += nums[i];
+            if (currentSum + nums[i] <= maxAllowedSum) {
+                currentSum += nums[i];
             } else {
-                partitions++;
-                subarraySum = nums[i];
+                subarrays++;
+                currentSum = nums[i];
             }
         }
 
-        return partitions;
+        return subarrays;
     }
-    public int largestSubArraySumMinimized(int[] nums, int k) {
 
-        // Initialize binary search boundaries
-        int low = nums[0];
-        int high = 0;
+    public static void main(String[] args) {
+        SplitArrayLargestSum_7 solution = new SplitArrayLargestSum_7();
 
-        for (int i = 0; i < nums.length; i++) {
-            low = Math.max(low, nums[i]);
-            high += nums[i];
-        }
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int partitions = countPartitions(nums, mid);
-
-            if (partitions > k) {
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
-        }
-
-        return low;
-    }
-    public static void main (String[] args) {
-        SplitArrayLargestSum_7 solver = new SplitArrayLargestSum_7();
-        int[] nums = {10, 20, 30, 40};
+        int[] nums = {7, 2, 5, 10, 8};
         int k = 2;
 
-        int result = solver.largestSubArraySumMinimized(nums, k);
-        System.out.println("The minimized largest sum of the split is: " + result);
+        int result = solution.splitArray(nums, k);
+        System.out.println("Minimum possible largest sub-array sum: " + result);
     }
 }
