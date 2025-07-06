fde8913d7da3761a99f9470f083dce6ffccec3f5
Yaswanth Gudivada <144692822+Yaswanth6303@users.noreply.github.com>
2025-07-06 15:31:45 +0530

    Edit SubSequence_1 Code

--- AdvancedProblems/Recursion/src/subsequencePatternProblems/SubSequenceSum_1.java
+++ AdvancedProblems/Recursion/src/subsequencePatternProblems/SubSequenceSum_1.java
@@ -1,23 +1,16 @@
 package subsequencePatternProblems;
 
 public class SubSequenceSum_1 {
-    public boolean solve(int i, int n, int[] nums, int k) {
-        // Base case: if the sum k is 0, a subsequence is found
-        if (k == 0) {
-            return true;
-        }
-        // Base case: if k is negative, no valid subsequence can be found
-        if (k < 0) {
-            return false;
-        }
-        // Base case: if all elements are processed, check if k is 0
-        if (i == n) {
-            return k == 0;
-        }
-        // Recursive call: include the current element in the subsequence
-        // or exclude the current element from the subsequence
-        return solve(i + 1, n, nums, k - nums[i]) || solve(i + 1, n, nums, k);
+    public boolean solve(int index, int n, int[] nums, int sum) {
+        if (sum == 0) return true;
+        if (index == n) return false;
+        if (sum < 0) return false;
+
+        boolean path1 = solve(index + 1, n, nums, sum - nums[index]);
+        boolean path2 = solve(index + 1, n, nums, sum);
+        return path1 || path2;
     }
+
     /**
      * Time Complexity: O(2^n)
      * Space Complexity: O(n)
@@ -28,8 +21,8 @@ public class SubSequenceSum_1 {
     }
     public static void main(String[] args) {
         SubSequenceSum_1 solution = new SubSequenceSum_1();
-        int[] nums = {1, 2, 3, 4};
-        int target = 8;
+        int[] nums = {1, 4, 3};
+        int target = 10;
         System.out.print("Array contains subsequence sum: ");
         System.out.println(solution.checkSubsequenceSum(nums, target));
     }
