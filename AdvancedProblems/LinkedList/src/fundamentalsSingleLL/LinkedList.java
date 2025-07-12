9ec6ef6d330d075acfb89ab64530676e418dde82
Yaswanth Gudivada <144692822+Yaswanth6303@users.noreply.github.com>
2025-07-12 15:30:54 +0530

    Add LinkedList Code

---
+++ AdvancedProblems/LinkedList/src/fundamentalsSingleLL/LinkedList.java
@@ -0,0 +1,65 @@
+package fundamentalsSingleLL;
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public Node() {
+        this.data = 0;
+        this.next = null;
+    }
+}
+
+public class LinkedList {
+
+    // Convert array to linked list
+    Node convertArrToLL(int[] nums) {
+        if (nums.length == 0) return null;
+
+        Node head = new Node(nums[0]);
+        Node mover = head;
+
+        for (int i = 1; i < nums.length; i++) {
+            Node temp = new Node(nums[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+
+        return head;
+    }
+
+    // Print linked list
+    void printLinkedList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 4, 6, 8};
+
+        LinkedList ll = new LinkedList();
+        Node head = ll.convertArrToLL(nums);
+        System.out.print("First Element in the LL: ");
+        System.out.println(head.data);
+
+        System.out.print("Second Element in the LL: ");
+        System.out.println(head.next.data);
+
+        System.out.println("Linked List:");
+        ll.printLinkedList(head);
+    }
+}
