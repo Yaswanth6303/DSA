52332b87712f2242b11207637ee2f2c9ee3f92e4
Yaswanth Gudivada <144692822+Yaswanth6303@users.noreply.github.com>
2025-08-08 20:09:20 +0530

    Add ReverseLinkedList code

---
+++ AdvancedProblems/LinkedList/src/logicBuilding/ReverseLinkedList_5.java
@@ -0,0 +1,73 @@
+package logicBuilding;
+
+import fundamentalsSingleLL.LinkedList;
+import fundamentalsSingleLL.Node;
+
+import java.util.Stack;
+
+public class ReverseLinkedList_5 {
+    public Node reverseListBrute(Node head) {
+        if (head == null) return null;
+
+        Stack<Node> stack = new Stack<>();
+        Node current = head;
+
+        while (current != null) {
+            stack.push(current);
+            current = current.next;
+        }
+
+        Node newHead = stack.pop();
+        current = newHead;
+
+        while (!stack.isEmpty()) {
+            current.next = stack.pop();
+            current = current.next;
+        }
+
+        current.next = null;
+
+        return newHead;
+    }
+
+    public Node reverseListOptimalIterative(Node head) {
+        Node current = head;
+        Node prev = null;
+
+        while (current != null) {
+            Node front = current.next;
+            current.next = prev;
+            prev = current;
+            current = front;
+        }
+
+        return prev;
+    }
+
+    public Node reverseListOptimalRecursive(Node head) {
+        if (head == null && head.next == null) return head;
+
+        Node newHead = reverseListOptimalRecursive(head.next);
+        Node front = head.next;
+        front.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ReverseLinkedList_5 rll = new ReverseLinkedList_5();
+
+        Node l1 = new Node(1, new Node(3, new Node(2, new Node(5))));
+        Node head = rll.reverseListBrute(l1);
+        ll.printLinkedList(head);
+
+        Node l2 = new Node(1, new Node(3, new Node(2, new Node(5))));
+        Node head1 = rll.reverseListOptimalIterative(l2);
+        ll.printLinkedList(head1);
+
+        Node l3 = new Node(1, new Node(3, new Node(2, new Node(5))));
+        Node head2 = rll.reverseListOptimalIterative(l3);
+        ll.printLinkedList(head2);
+    }
+}
