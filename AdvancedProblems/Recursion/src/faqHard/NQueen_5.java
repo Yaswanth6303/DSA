99c179fae4fff1ab4cc2ac10850964ee85870fc6
Yaswanth Gudivada <yswnthdev@gmail.com>
2025-10-30 15:26:34 +0530

    Edit NQueen_5 code
    Change from for to while

--- AdvancedProblems/Recursion/src/faqHard/NQueen_5.java
+++ AdvancedProblems/Recursion/src/faqHard/NQueen_5.java
@@ -33,19 +33,37 @@ public class NQueen_5 {
     }
 
     private boolean isSafe(char[][] board, int row, int col, int n) {
-        // Check column
-        for (int i = 0; i < row; i++) {
-            if (board[i][col] == 'Q') return false;
+        int r, c;
+
+        // Check column (above current row)
+        r = row - 1;
+        while (r >= 0) {
+            if (board[r][col] == 'Q') {
+                return false;
+            }
+            r--;
         }
 
         // Check upper-left diagonal
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') return false;
+        r = row - 1;
+        c = col - 1;
+        while (r >= 0 && c >= 0) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+            r--;
+            c--;
         }
 
         // Check upper-right diagonal
-        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
-            if (board[i][j] == 'Q') return false;
+        r = row - 1;
+        c = col + 1;
+        while (r >= 0 && c < n) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+            r--;
+            c++;
         }
 
         return true;
