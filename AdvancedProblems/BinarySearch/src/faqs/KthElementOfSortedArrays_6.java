71e7f1b07bcefe15f010461cbb14e98eada73024
Yaswanth Gudivada <yswnthdev@gmail.com>
2025-11-11 15:45:23 +0530

    Edit forLoop

--- AdvancedProblems/BinarySearch/src/faqs/KthElementOfSortedArrays_6.java
+++ AdvancedProblems/BinarySearch/src/faqs/KthElementOfSortedArrays_6.java
@@ -99,12 +99,12 @@ public class KthElementOfSortedArrays_6 {
             int r1 = (mid1 == n1) ? Integer.MAX_VALUE : nums1[mid1];
             int r2 = (mid2 == n2) ? Integer.MAX_VALUE : nums2[mid2];
 
-            if (l1 <= r2 && l2 <= r1) {
-                return Math.max(l1, l2);
-            } else if (l1 > r2) {
+            if (l1 > r2) {
                 high = mid1 - 1;
-            } else {
+            } else if (l2 > r1){
                 low = mid1 + 1;
+            } else {
+                return Math.max(l1, l2);
             }
         }
 
