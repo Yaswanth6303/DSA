5545f93903263daafcbdbe0c5ffc6a2e31430f99
Yaswanth Gudivada <yswnthdev@gmail.com>
2025-12-22 18:35:32 +0530

    Add Better and Optimal Approach

--- AdvancedProblems/SlidingWindow/src/longestandsmallestwindowproblems/KDistinctCharacters_4.java
+++ AdvancedProblems/SlidingWindow/src/longestandsmallestwindowproblems/KDistinctCharacters_4.java
@@ -27,11 +27,81 @@ public class KDistinctCharacters_4 {
 
         return maxLength;
     }
+
+    /**
+     * Time Complexity: O(2n)
+     * Space Complexity: O(k)
+     */
+    public int kDistinctCharactersBetter(String str, int k) {
+        int n = str.length();
+
+        int l = 0;
+        int maxLength = 0;
+
+        HashMap<Character, Integer> mpp = new HashMap<>();
+
+        for (int r = 0; r < n; r++) {
+            char ch = str.charAt(r);
+            mpp.put(ch, mpp.getOrDefault(ch, 0) + 1);
+
+            while (mpp.size() > k) {
+                char leftChar = str.charAt(l);
+                mpp.put(leftChar, mpp.get(leftChar) - 1);
+                if (mpp.get(leftChar) == 0) {
+                    mpp.remove(leftChar);
+                }
+                l++;
+            }
+
+            maxLength = Math.max(maxLength, r - l + 1);
+        }
+
+        return maxLength;
+    }
+
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(k)
+     */
+    public int kDistinctCharactersOptimal(String str, int k) {
+        int n = str.length();
+
+        int l = 0;
+        int maxLength = 0;
+
+        HashMap<Character, Integer> mpp = new HashMap<>();
+
+        for (int r = 0; r < n; r++) {
+            char ch = str.charAt(r);
+            mpp.put(ch, mpp.getOrDefault(ch, 0) + 1);
+
+            if (mpp.size() > k) {
+                char leftChar = str.charAt(l);
+                mpp.put(leftChar, mpp.get(leftChar) - 1);
+                if (mpp.get(leftChar) == 0) {
+                    mpp.remove(leftChar);
+                }
+                l++;
+            }
+
+            maxLength = Math.max(maxLength, r - l + 1);
+        }
+
+        return maxLength;
+    }
+
     static void main() {
         String str = "aaabbccd";
         int k = 2;
-        KDistinctCharacters_4 objBrute = new KDistinctCharacters_4();
-        int lengthBrute = objBrute.kDistinctCharactersBrute(str, k);
-        System.out.println("Maximum length of substring with at most " + k + " distinct characters: " + lengthBrute);
+
+        KDistinctCharacters_4 obj = new KDistinctCharacters_4();
+        int lengthBrute = obj.kDistinctCharactersBrute(str, k);
+        System.out.println("Maximum length of substring with at most " + k + " distinct characters(Brute): " + lengthBrute);
+
+        int lengthBetter = obj.kDistinctCharactersBetter(str, k);
+        System.out.println("Maximum length of substring with at most " + k + " distinct characters(Better): " + lengthBetter);
+
+        int lengthOptimal = obj.kDistinctCharactersOptimal(str, k);
+        System.out.println("Maximum length of substring with at most " + k + " distinct characters(Optimal): " + lengthOptimal);
     }
 }
