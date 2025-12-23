e83bfdd9dc0ee272613f26107ff43275715442c7
Yaswanth Gudivada <yswnthdev@gmail.com>
2025-12-23 19:58:53 +0530

    Add Optimal Approach

--- AdvancedProblems/SlidingWindow/src/longestandsmallestwindowproblems/MinimumWindowSubstring_6.java
+++ AdvancedProblems/SlidingWindow/src/longestandsmallestwindowproblems/MinimumWindowSubstring_6.java
@@ -1,7 +1,11 @@
 package longestandsmallestwindowproblems;
 
 public class MinimumWindowSubstring_6 {
-    public String minWindow(String s, String t) {
+    /**
+     * Time Complexity: O(n^2)
+     * Space Complexity: O(256)
+     */
+    public String minWindowBrute(String s, String t) {
         int sLength = s.length();
         int tLength = t.length();
 
@@ -34,13 +38,59 @@ public class MinimumWindowSubstring_6 {
 
         return (startIndex == -1) ? "" : s.substring(startIndex, startIndex + minLength);
     }
+    /**
+     * Time Complexity: O(2n + m)
+     * Space Complexity: O(256)
+     */
+    public String minWindowOptimal(String s, String t) {
+        int sLen = s.length();
+        int tLen = t.length();
+
+        int minLength = Integer.MAX_VALUE;
+        int startIndex = -1;
+
+        int[] hash = new int[256];
+        for (char ch : t.toCharArray()) {
+            hash[ch]++;
+        }
+
+        int l = 0;
+        int count = 0;
+
+        for (int r = 0; r < sLen; r++) {
+            if (hash[s.charAt(r)] > 0) {
+                count++;
+            }
+            hash[s.charAt(r)]--;
+
+            while (count == tLen) {
+                int strLength = r - l + 1;
+                if (strLength < minLength) {
+                    minLength = strLength;
+                    startIndex = l;
+                }
+
+                hash[s.charAt(l)]++;
+                if (hash[s.charAt(l)] > 0) {
+                    count--;
+                }
+                l++;
+            }
+        }
+        return (startIndex == -1) ? "" : s.substring(startIndex, startIndex + minLength);
+
+    }
+
     static void main() {
         MinimumWindowSubstring_6 obj = new MinimumWindowSubstring_6();
 
         String s = "ddaaabbca";
         String t = "abc";
-        String result = obj.minWindow(s, t);
 
-        System.out.println("Minimum length substring containing all characters from \"" + t + "\" is: " + result);
+        String resultBrute = obj.minWindowBrute(s, t);
+        System.out.println("Minimum length substring containing all characters from \"" + t + "\" is(Brute): " + resultBrute);
+
+        String resultOptimal = obj.minWindowOptimal(s, t);
+        System.out.println("Minimum length substring containing all characters from \"" + t + "\" is(Optimal): " + resultOptimal);
     }
 }
