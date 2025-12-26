6d1ea78c0fad70d24df4d2ba1686882e4c89be5d
Yaswanth Gudivada <yswnthdev@gmail.com>
2025-12-26 20:10:20 +0530

    Add Binary Subarray Sum optimal code

--- AdvancedProblems/SlidingWindow/src/substringProblems/BinarySubarraysWithSum_2.java
+++ AdvancedProblems/SlidingWindow/src/substringProblems/BinarySubarraysWithSum_2.java
@@ -1,6 +1,10 @@
 package substringProblems;
 
 public class BinarySubarraysWithSum_2 {
+    /**
+     * Time Complexity: O(n^2)
+     * Space Complexity: O(1)
+     */
     public int numSubarraysWithSumBrute(int[] nums, int goal) {
         int n = nums.length;
         int count = 0;
@@ -17,11 +21,44 @@ public class BinarySubarraysWithSum_2 {
 
         return count;
     }
+
+    private int numSubarraysWithSumLessEqualToGoal(int[] nums, int goal) {
+        if (goal < 0) return 0;
+
+        int n = nums.length;
+        int l = 0;
+        int sum = 0;
+        int count = 0;
+
+        for (int r = 0; r < n; r++) {
+            sum += nums[r];
+
+            while (sum > goal) {
+                sum -= nums[l];
+                l++;
+            }
+
+            count += (r - l + 1);
+        }
+
+        return count;
+    }
+
+    /**
+     * Time Complexity: O(2 * 2n)
+     * Space Complexity: O(1)
+     */
+    public int numSubarraysWithSumOptimal(int[] nums, int goal) {
+        return numSubarraysWithSumLessEqualToGoal(nums, goal) - numSubarraysWithSumLessEqualToGoal(nums, goal - 1);
+    }
+
     static void main() {
         BinarySubarraysWithSum_2 obj = new BinarySubarraysWithSum_2();
         int[] nums = {1, 0, 1, 0, 1};
         int goal = 2;
-        int result = obj.numSubarraysWithSumBrute(nums, goal);
-        System.out.println("Number of substrings with sum \"" + goal + "\" is: " + result);
+        int resultBrute = obj.numSubarraysWithSumBrute(nums, goal);
+        int resultOptimal = obj.numSubarraysWithSumOptimal(nums, goal);
+        System.out.println("Number of substrings with sum \"" + goal + "\" is(Brute): " + resultBrute);
+        System.out.println("Number of substrings with sum \"" + goal + "\" is(Optimal): " + resultOptimal);
     }
 }
